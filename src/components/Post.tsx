40ddd159a9badb798dfc5cd5f587f6e08a687d36
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-10 18:08:16 -0300

    Refactorizando el footer y mejorando la vista del contenedor

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,17 +1,34 @@
 import * as React from 'react';
 
+import { faThumbsUp, faRetweet } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const style = {
     backgroundColor: '#fff',
     border: '1px solid #ddd',
     padding: '10px 15px',
 }
 
-export default class Post extends React.Component {
+interface IPostProps {
+    image: string
+}
+
+export default class Post extends React.Component<IPostProps> {
     public render() {
-        const { children } = this.props
+        const { image } = this.props
         return (
             <div style={style}>
-                {children}
+                <img src={image} alt='sad' />
+                <div style={{ display: 'flex', backgroundColor: '#eee', marginLeft: '-15px', marginBottom: '-10px', width: 'calc(100% + 30px)' }}>
+                    <div style={{ flex: 1, textAlign: 'center', padding: '10px 15px', cursor: 'pointer' }}>
+                        <FontAwesomeIcon icon={faThumbsUp} />
+                        Like
+                    </div>
+                    <div style={{ flex: 1, textAlign: 'center', padding: '10px 15px', cursor: 'pointer' }}>
+                        <FontAwesomeIcon icon={faRetweet} />
+                        Share
+                    </div>
+                </div>
             </div>
         )
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router'
-import createHistory from 'history/createBrowserHistory'
+import { createBrowserHistory } from 'history'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const history = createHistory()
+const history = createBrowserHistory()
 ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
