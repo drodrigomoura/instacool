bbe9b46b5a7025773b5a751dff9911a7cbcac2d3
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-12 13:46:15 -0300

    Configurando firebase e injectando dpndncias a los thunks

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,10 @@ import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import * as reducers from './ducks'
 import thunk from 'redux-thunk'
-
+import services from './services'
 const store = createStore(combineReducers({
   ...reducers
-}), applyMiddleware(thunk))
+}), applyMiddleware(thunk.withExtraArgument(services)))
 const history = createBrowserHistory()
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCYSyHk3f0WnTrJw16cZZg0butp_4h3Kso",
+    appId: "1:63898009827:web:8cb06308e7b318c3bc45d6",
+    authDomain: "instacool-8eeb8.firebaseapp.com",
+    databaseURL: "https://instacool-8eeb8.firebaseio.com",
+    measurementId: "G-GYHQYSSL69",
+    messagingSenderId: "63898009827",
+    projectId: "instacool-8eeb8",
+    storageBucket: "instacool-8eeb8.appspot.com",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
+export const storage = firebase.storage()
