08d54692a8a23ebb34964e12773da4762dfa054e
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-12 15:29:19 -0300

    thunk para login y tipos para servicios

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,14 @@
 import * as firebase from 'firebase'
+import { firestore, storage, auth } from 'firebase'
 
 const services = {
     ...firebase,
+}
+
+export interface IServices {
+    auth: auth.Auth
+    db: firestore.Firestore
+    storage: storage.Storage
 }
 
 export default services
