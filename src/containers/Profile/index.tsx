6e730329fae5866411b4b17d4fb653b4b4359b95
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-12 11:57:51 -0300

    Listado de imagenes posteadas por el usuario

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react'
 import ProfileImg from '../../components/ProfileImg'
 import Button from '../../components/Button'
+import Card from '../../components/Card'
 
 const style = {
     container: {
         padding: '15px',
+    },
+    row: {
         display: 'flex',
         justifyContent: 'space-between',
+        marginBottom: '10px'
     }
 }
 
@@ -14,9 +18,22 @@ export default class Profile extends React.Component {
     public render() {
         return (
             <div style={style.container}>
-                <ProfileImg />
-                <Button>Agregar</Button>
+                <div style={style.row}>
+                    <ProfileImg />
+                    <Button>Agregar</Button>
+                </div>
+                <div style={style.row}>
+                    <Card><img src='http://placekitten.com/100/100' alt='img' /></Card>
+                    <Card><img src='http://placekitten.com/100/100' alt='img' /></Card>
+                    <Card><img src='http://placekitten.com/100/100' alt='img' /></Card>
+                </div>
+                <div style={style.row}>
+                    <Card><img src='http://placekitten.com/100/100' alt='img' /></Card>
+                    <Card><img src='http://placekitten.com/100/100' alt='img' /></Card>
+                    <Card><img src='http://placekitten.com/100/100' alt='img' /></Card>
+                </div>
             </div>
+
         )
     }
 }
