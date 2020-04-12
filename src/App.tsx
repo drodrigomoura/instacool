54f0a33179e12dc11eaca3ab2de517266c3acf6f
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-12 11:29:31 -0300

    creando ruta y contenedor para profile

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route } from 'react-router'
 import Login from './containers/Auth/Login';
 import Register from './containers/Auth/Register';
 import NewsFeed from './containers/NewsFeed/';
+import Profile from './containers/Profile';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route exact path='/register' component={Register} />
       <Route path='/app' component={Navbar} />
       <Route exact path='/app/newsfeed' component={NewsFeed} />
+      <Route exact path='/app/profile' component={Profile} />
     </div>
   );
 }
---
+++ src/containers/Profile/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+const style = {
+    container: {
+        padding: '15px',
+    }
+}
+
+export default class Profile extends React.Component {
+    public render() {
+        return (
+            <div style={style.container}>
+                lala
+            </div>
+        )
+    }
+}
