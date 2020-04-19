2321a1b4dff51df3c7f92a61b2ba7fe7bba02add
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-19 11:28:45 -0300

    Middleware de autenticacion

---
+++ functions/src/createServer.ts
@@ -0,0 +1,36 @@
+import * as express from 'express'
+import * as admin from 'firebase-admin'
+
+const db = admin.firestore()
+const auth = admin.auth()
+
+export default () => {
+    const app = express()
+    app.use(async (req, res, next) => {
+        const token = req.headers.authorization
+        try {
+            const { uid, email } = await auth.verifyIdToken(token)
+            const snap = await db.collection('users').doc(uid).get()
+            const user = snap.data()
+            Object.assign(req, {
+                user: {
+                    ...user,
+                    uid,
+                    email
+                }
+            })
+            next()
+        } catch (e) {
+            res.status(403).send("Error al autorizar")
+        }
+    })
+
+    app.get('/post', (req, res) => {
+
+        res.send("Hola mundo desde express")
+    })
+
+
+
+    return app
+}
