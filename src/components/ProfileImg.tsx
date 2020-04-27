b5938079c678e6338e609d60f3378b27705129bd
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-27 17:39:37 -0300

    Cambiando la imagen de perfil del usuario, creando el formulario

--- src/components/ProfileImg.tsx
+++ src/components/ProfileImg.tsx
@@ -1,17 +1,42 @@
 import * as React from 'react'
+import { reduxForm, InjectedFormProps, Field, WrappedFieldProps, WrappedFieldInputProps } from 'redux-form'
 
 const style = {
+    file: {
+        display: 'none',
+    },
     img: {
         borderRadius: '100%',
-    }
+    },
 }
 
-export default class ProfileImg extends React.Component {
+const handleChange = (input: WrappedFieldInputProps) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const { onChange } = input
+    const { files } = e.target
+    if (files) { onChange(files[0]) }
+}
+
+const RenderField: React.StatelessComponent<WrappedFieldProps> = ({ input }) =>
+    <div>
+        <input onChange={handleChange(input)} style={style.file} type='file' id='profileImage' />
+        <label htmlFor='profileImage'>
+            <img style={style.img} src='http://placekitten.com/100/100' alt="profile img" />
+        </label>
+    </div>
+
+class ProfileImg extends React.Component<InjectedFormProps> {
     public render() {
+        const { handleSubmit } = this.props
         return (
-            <div >
-                <img style={style.img} src='http://placekitten.com/100/100' alt="profile img" />
-            </div>
+            <form onSubmit={handleSubmit}>
+                <Field name='file' component={RenderField} />
+
+            </form>
         )
     }
 }
+
+export default reduxForm({
+    form: 'profileImg'
+})(ProfileImg)
