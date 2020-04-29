5a4ec4e127bfbc8da62dd403d6af064e867a4f55
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-29 17:00:28 -0300

    problema con typescript (nuevamente ).

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,96 +1,104 @@
-import * as React from 'react'
-import ProfileImg from '../../components/ProfileImg'
-import Button from '../../components/Button'
-import Card from '../../components/Card'
-import * as postsDuck from '../../ducks/Posts'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { ThunkDispatch } from 'redux-thunk'
-import services from '../../services'
-import { chunk } from 'lodash'
-import { submit } from 'redux-form'
+import * as React from "react";
+import ProfileImg from "../../components/ProfileImg";
+import Button from "../../components/Button";
+import Card from "../../components/Card";
+import * as postsDuck from "../../ducks/Posts";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { ThunkDispatch } from "redux-thunk";
+import services from "../../services";
+import { chunk } from "lodash";
+import { submit } from "redux-form";
 
-const { auth } = services
+const { auth } = services;
 
 const style = {
-    container: {
-        padding: '15px',
-    },
-    row: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        marginBottom: '10px'
-    },
-    img: {
-        width: '100px'
-    }
-}
-
+  container: {
+    padding: "15px",
+  },
+  row: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginBottom: "10px",
+  },
+  img: {
+    width: "100px",
+  },
+};
 
 interface IProfileProps {
-    fetchPosts: () => void,
-    submitProfileImg: () => void,
-    fetched: boolean,
-    loading: boolean,
-    data: postsDuck.IPost[][]
+  fetchPosts: () => void;
+  submitProfileImg: () => void;
+  handleProfileImageSubmit: (a: { file: File }) => void;
+  fetched: boolean;
+  loading: boolean;
+  data: postsDuck.IPost[][];
 }
 
 class Profile extends React.Component<IProfileProps> {
-    constructor(props: IProfileProps) {
-        super(props)
-        const { fetchPosts, fetched } = props
-        if (fetched) {
-            return
-        }
-        fetchPosts()
+  constructor(props: IProfileProps) {
+    super(props);
+    const { fetchPosts, fetched } = props;
+    if (fetched) {
+      return;
     }
-    public render() {
-        const { data, submitProfileImg } = this.props
-        return (
-            <div style={style.container}>
-                <div style={style.row}>
-                    <ProfileImg submitProfileImg={submitProfileImg} />
-                    <Button>Agregar</Button>
-                </div>
-                {data.map((x, i) =>
-                    <div key={i} style={style.row}>
-                        {x.map(y =>
-                            <Card><img src={y.imageURL} style={style.img} alt='img' /></Card>
-                        )}
-                    </div>
-                )}
-
-
-            </div>
-
-        )
-    }
+    fetchPosts();
+  }
+  public render() {
+    const { data, submitProfileImg, handleProfileImageSubmit } = this.props;
+    return (
+      <div style={style.container}>
+        <div style={style.row}>
+          <ProfileImg
+            onSubmit={handleProfileImageSubmit}
+            submitProfileImg={submitProfileImg}
+          />
+          <Button>Agregar</Button>
+        </div>
+        {data.map((x, i) => (
+          <div key={i} style={style.row}>
+            {x.map((y) => (
+              <Card>
+                <img src={y.imageURL} style={style.img} alt="img" />
+              </Card>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: any) => {
-    console.log('mapstatetoprops', state);
+  console.log("mapstatetoprops", state);
 
-    const { Post: { data, fetching, fetched } } = state
-    const loading = fetching || !fetched
-    console.log(auth.currentUser && auth.currentUser.uid)
+  const {
+    Post: { data, fetching, fetched },
+  } = state;
+  const loading = fetching || !fetched;
+  console.log(auth.currentUser && auth.currentUser.uid);
 
-    const filtered = Object.keys(data).reduce((acc, el) => {
-        if (data[el].userId !== (auth.currentUser && auth.currentUser.uid)) {
-            return acc
-        }
-        return acc.concat(data[el])
-    }, [] as postsDuck.IPost[])
-
-    return {
-        data: chunk(filtered, 3),
-        fetched,
-        loading,
+  const filtered = Object.keys(data).reduce((acc, el) => {
+    if (data[el].userId !== (auth.currentUser && auth.currentUser.uid)) {
+      return acc;
     }
-}
+    return acc.concat(data[el]);
+  }, [] as postsDuck.IPost[]);
 
-const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, any>) => bindActionCreators({
-    ...postsDuck,
-    submitProfileImg: () => submit('profileImg')
-}, dispatch)
+  return {
+    data: chunk(filtered, 3),
+    fetched,
+    loading,
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile)
+const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, any>) =>
+  bindActionCreators(
+    {
+      ...postsDuck,
+      submitProfileImg: () => submit("profileImg"),
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
--- src/ducks/Posts.ts
+++ src/ducks/Posts.ts
@@ -1,151 +1,175 @@
-import { Dispatch, AnyAction } from "redux"
-import { IServices } from "../services"
-import { firestore } from "firebase"
-import { download } from '../utils'
+import { Dispatch, AnyAction } from "redux";
+import { IServices } from "../services";
+import { firestore } from "firebase";
+import { download } from "../utils";
 
-const START = 'posts/fetch-start'
-const SUCCESS = 'posts/fetch-success'
-const ERROR = 'posts/fetch-error'
-const ADD = 'posts/add'
+const START = "posts/fetch-start";
+const SUCCESS = "posts/fetch-success";
+const ERROR = "posts/fetch-error";
+const ADD = "posts/add";
 
 export interface IPost {
-    comment: string,
-    userId: string,
-    createdAt: firestore.Timestamp,
-    imageURL: string
+  comment: string;
+  userId: string;
+  createdAt: firestore.Timestamp;
+  imageURL: string;
 }
 export interface IDataPosts {
-    [key: string]: IPost
+  [key: string]: IPost;
 }
 
 const fetchStart = () => ({
-    type: START,
-})
+  type: START,
+});
 
 const fetchSuccess = (payload: IDataPosts) => ({
-    type: SUCCESS,
-    payload,
-}
-)
+  type: SUCCESS,
+  payload,
+});
 const fetchError = (error: Error) => ({
-    type: ERROR,
-    error,
-})
+  type: ERROR,
+  error,
+});
 
 const add = (payload: IDataPosts) => ({
-    payload,
-    type: ADD
-})
+  payload,
+  type: ADD,
+});
 
 const initialState = {
-    data: {},
-    fetched: false,
-    fetching: false,
-}
+  data: {},
+  fetched: false,
+  fetching: false,
+};
 
 export default function reducer(state = initialState, action: AnyAction) {
-    switch (action.type) {
-        case START:
-            return {
-                ...state,
-                fetching: true
-            }
-        case SUCCESS:
-            return {
-                ...state,
-                data: action.payload,
-                fetched: true,
-                fetching: false
-            }
-        case ERROR:
-            return {
-                ...state,
-                error: action.error,
-                fetching: false
-            }
-        case ADD:
-            return {
-                ...state,
-                data: {
-                    ...state.data,
-                    ...action.payload
-                }
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case START:
+      return {
+        ...state,
+        fetching: true,
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        data: action.payload,
+        fetched: true,
+        fetching: false,
+      };
+    case ERROR:
+      return {
+        ...state,
+        error: action.error,
+        fetching: false,
+      };
+    case ADD:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          ...action.payload,
+        },
+      };
+    default:
+      return state;
+  }
 }
 
-export const fetchPosts = () =>
-    async (dispatch: Dispatch, getState: () => any, { db, storage }: IServices) => {
-        dispatch(fetchStart())
-        try {
-            const snaps = await db.collection('posts').orderBy('createdAt', 'desc').get()
-            const posts: { [index: string]: any } = {}
-            snaps.forEach((x) => posts[x.id] = x.data())
+export const fetchPosts = () => async (
+  dispatch: Dispatch,
+  getState: () => any,
+  { db, storage }: IServices
+) => {
+  dispatch(fetchStart());
+  try {
+    const snaps = await db
+      .collection("posts")
+      .orderBy("createdAt", "desc")
+      .get();
+    const posts: { [index: string]: any } = {};
+    snaps.forEach((x) => (posts[x.id] = x.data()));
 
+    const imgIds = await Promise.all(
+      Object.keys(posts).map(async (x) => {
+        const ref = storage.ref(`posts/${x}.jpg`);
+        const url = await ref.getDownloadURL();
+        console.log(url);
 
-            const imgIds = await Promise.all(Object.keys(posts)
-                .map(async x => {
-                    const ref = storage.ref(`posts/${x}.jpg`)
-                    const url = await ref.getDownloadURL()
-                    console.log(url);
+        return [x, url];
+      })
+    );
 
-                    return [x, url]
-                }))
+    const keyedImages: { [index: string]: any } = {};
+    imgIds.forEach((x) => (keyedImages[x[0]] = x[1]));
+    // console.log('keyedImages', keyedImages);
 
-            const keyedImages: { [index: string]: any } = {}
-            imgIds.forEach(x => keyedImages[x[0]] = x[1])
-            // console.log('keyedImages', keyedImages);
-
-            Object.keys(posts).forEach(x => posts[x] = {
-                ...posts[x],
-                imageURL: keyedImages[x]
-            })
-
-            console.log(posts);
-
-            dispatch(fetchSuccess(posts))
-        } catch (e) {
-            dispatch(fetchError(e))
-        }
-    }
-
-export const like = (id: string) =>
-    async (dispatch: Dispatch, getState: () => any, { auth }: IServices) => {
-        if (!auth.currentUser) return
-        const token = await auth.currentUser.getIdToken()
-        await fetch(`/api/posts/${id}/like`, {
-            headers: {
-                authorization: token
-            }
+    Object.keys(posts).forEach(
+      (x) =>
+        (posts[x] = {
+          ...posts[x],
+          imageURL: keyedImages[x],
         })
-    }
+    );
 
-export const share = (id: string) =>
-    async (dispatch: Dispatch, getState: () => any, { auth, db, storage }: IServices) => {
-        if (!auth.currentUser) return
-        const token = await auth.currentUser.getIdToken()
-        const result = await fetch(`/api/posts/${id}/share`, {
-            headers: {
-                authorization: token
-            }
-        })
+    console.log(posts);
 
-        const url = await storage.ref(`posts/${id}.jpg`).getDownloadURL()
-        const blob = await download(url) as Blob
+    dispatch(fetchSuccess(posts));
+  } catch (e) {
+    dispatch(fetchError(e));
+  }
+};
 
-        const { id: postId }: { id: string } = await result.json()
-        const ref = storage.ref(`posts/${postId}.jpg`)
+export const like = (id: string) => async (
+  dispatch: Dispatch,
+  getState: () => any,
+  { auth }: IServices
+) => {
+  if (!auth.currentUser) return;
+  const token = await auth.currentUser.getIdToken();
+  await fetch(`/api/posts/${id}/like`, {
+    headers: {
+      authorization: token,
+    },
+  });
+};
 
-        await ref.put(blob)
-        const imageURL = await ref.getDownloadURL()
-        const snap = await db.collection('posts').doc(postId).get()
+export const share = (id: string) => async (
+  dispatch: Dispatch,
+  getState: () => any,
+  { auth, db, storage }: IServices
+) => {
+  if (!auth.currentUser) return;
+  const token = await auth.currentUser.getIdToken();
+  const result = await fetch(`/api/posts/${id}/share`, {
+    headers: {
+      authorization: token,
+    },
+  });
 
-        dispatch(add({
-            [snap.id]: {
-                ...snap.data(),
-                imageURL
-            }
-        } as IDataPosts))
-    }
+  const url = await storage.ref(`posts/${id}.jpg`).getDownloadURL();
+  const blob = (await download(url)) as Blob;
+
+  const { id: postId }: { id: string } = await result.json();
+  const ref = storage.ref(`posts/${postId}.jpg`);
+
+  await ref.put(blob);
+  const imageURL = await ref.getDownloadURL();
+  const snap = await db.collection("posts").doc(postId).get();
+
+  dispatch(
+    add({
+      [snap.id]: {
+        ...snap.data(),
+        imageURL,
+      },
+    } as IDataPosts)
+  );
+};
+
+export const handleProfileImageSubmit = (payload: { file: File }) => (
+  dispatch: Dispatch,
+  getState: () => any,
+  {}: IServices
+) => {
+  console.log(payload);
+};
